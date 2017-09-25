a4d25f19c709bdd86737b64e629fcee77bb875fd
Santiago Bernhardt <santiago.bernhardt@xero.com>
2017-09-25 14:12:08 +1300

    making it nicer

--- Functions/Invoke-AWSTagEasy.ps1
+++ Functions/Invoke-AWSTagEasy.ps1
@@ -1,81 +1,63 @@
 ﻿# http://docs.aws.amazon.com/powershell/latest/reference/index.html?page=AWS_Resource_Groups_Tagging_API_cmdlets.html&tocid=AWS_Resource_Groups_Tagging_API_cmdlets
 
-# examples
-<# 
-
-old... $tags = '@{ "tagged-by"="powershell-tag-editor"; "portfolio"="fweb" }'
-
-$tags = @'
-tagged-by = powershell-tag-editor
-portfolio = fweb
-'@
-
-#>
-
 function Invoke-AWSTagEasy {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$region = (Get-EC2InstanceMetadata -Category Region | Select -ExpandProperty SystemName),
+  param(
+    [string]$region = (Get-EC2InstanceMetadata -Category Region | Select -ExpandProperty SystemName),
+    [string]$tagkey,
+    [string]$tagvalue,
+    [switch]$addtag,
+    [switch]$removetag,
+    [switch]$showresources
+  )
 
-        [string]$tags,
-
-        [string]$filterkey,
-        [string]$filtervalue,
-
-        [switch]$addtag,
-        [switch]$removetag
-    )
-
-    process{
-
-        $tagsresource = ConvertFrom-StringData -StringData $tags
-
-        if($filtervalue -and -not $filterkey){
-            Write-Warning "Can't use filtervalue without filterkey"
-            break
-        }
-
-        if($filterkey -and -not $filtervalue){
-            Write-Warning "Using key $filterkey"
-            $resources = Get-RGTResource -TagFilter @{ Key="$filterkey" } -Region $region
-        }
-        elseif($filterkey -and $filtervalue){
-            Write-Warning "Using key $filterkey value $filtervalue"
-            $resources = Get-RGTResource -TagFilter @{ Key="$filterkey"; Values=@("$filtervalue") } -Region $region
-        }
-        else{
-            $resources = Get-RGTResource -Region $region
-        }
-        
-
-        # ADD/UPDATE tags
-        if($addtag){
-          foreach($r in $resources){
-                $r.ResourceARN | Add-RGTResourceTag -Tag $tagsresource -Force -Region $region -Verbose
-                Start-Sleep -Seconds 1
-            }
-        }
-
-        # REMOVE tags
-        if($removetag){
-        foreach($r in $resources){
-               $r.ResourceARN | Remove-RGTResourceTag -TagKey @($tagsresource.keys) -Force -Region $region
-               Start-Sleep -Seconds 1
-            }
-        }
-
-        Write-Warning "Total resources found $(($resources).count)"
-        Write-Warning "Checking tagkeys... $($tagsresource.keys)"
-        foreach($tkey in $tagsresource.keys){
-            $tagged = Get-RGTTagValue -Key $tkey -Region $region
-            Write-Warning "TagKey $tkey TagValues found: $(($tagged).count)"
-            $tagged
-        }
-        
-        $continue = Read-Host "Show resources? [y/n]"
-        if($continue -eq 'y'){
-            $resources.ResourceARN
-        }
-
+  process{
+    # GET resources
+    if($tagkey -and -not $tagvalue){
+      $cando = "not-allowed"
+      Write-Warning "Using key $tagkey..."
+      $resources = Get-RGTResource -TagFilter @{ Key="$tagkey" } -Region $region
     }
+    elseif($tagkey -and $tagvalue){
+      $cando = "allowed"
+      Write-Warning "Using key $tagkey value $tagvalue..."
+      $resources = Get-RGTResource -TagFilter @{ Key="$tagkey"; Values=@("$tagvalue") } -Region $region
+    }
+    else{
+      $cando = "not-allowed"
+      Write-Warning "Getting all resources..."
+      $resources = Get-RGTResource -Region $region
+    }
+
+    # ADD/UPDATE tags
+    if($addtag -and $cando -eq "allowed"){
+      foreach($r in $resources){
+        $r.ResourceARN | Add-RGTResourceTag -Tag @{ Key="$tagkey"; Values=@("$tagvalue") } -Force -Region $region -Verbose
+        Start-Sleep -Seconds 1
+      }
+    }
+    elseif($addtag -and $cando -eq "not-allowed"){
+      Write-Warning "Command -addtag requires -tagvalue -tagkey"
+      break
+    }
+
+    # REMOVE tags
+    if($removetag -and $cando -eq "allowed"){
+      foreach($r in $resources){
+        Write-Warning "Removing tag:$tagkey on $(($r).ResourceARN)"
+        $r.ResourceARN | Remove-RGTResourceTag -TagKey @{ Key="$tagkey" } -Force -Region $region
+        Start-Sleep -Seconds 1
+      }
+    }
+    elseif($removetag -and $cando -eq "not-allowed"){
+      Write-Warning "Command -removetag requires -tagvalue -tagkey"
+      break
+    }
+
+    $tagged = Get-RGTTagValue -Key $tagkey -Region $region
+    Write-Warning "Total resources found $(($resources).count) where found $(($tagged).count) $tagkey.value"
+    $tagged
+    if($showresources){
+      $resources.ResourceARN
+    }
+  }
 }
