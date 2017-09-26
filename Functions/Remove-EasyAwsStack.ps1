e469a1d9a62b67e0b5e5f75db2e1442b2ca6f60d
Santiago Bernhardt <santiago.bernhardt@xero.com>
2017-09-27 11:54:34 +1300

    removal needs diferent order

--- Functions/Remove-EasyAwsStack.ps1
+++ Functions/Remove-EasyAwsStack.ps1
@@ -15,9 +15,7 @@ function Remove-EasyAwsStack {
     start-sleep -s 5
     #launch config
       Remove-ASLaunchConfiguration -LaunchConfigurationName lconfig-$serverclass -PassThru -Force -Region $region
-    #securitygroups
       start-sleep -s 20
-      Get-EC2SecurityGroup -Region $region | where {$_.GroupName -like "*$serverclass"} | Remove-EC2SecurityGroup -PassThru -Force -Region $region
     #iam instance profile
       Remove-IAMRoleFromInstanceProfile -InstanceProfileName iam-profile-$serverclass -RoleName iam-role-$serverclass -Force -PassThru -Region $region
     #iam policy
@@ -26,5 +24,8 @@ function Remove-EasyAwsStack {
       Remove-IAMRole -RoleName iam-role-$serverclass -Force -Region $region
     #instance profile
       Remove-IAMInstanceProfile -InstanceProfileName iam-profile-$serverclass -PassThru -Force -Region $region
+    #securitygroups
+      start-sleep -s 20
+      Get-EC2SecurityGroup -Region $region | where {$_.GroupName -like "*$serverclass"} | Remove-EC2SecurityGroup -PassThru -Force -Region $region
   } # close process
 } # close function
